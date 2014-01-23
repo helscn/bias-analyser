e17660dbe18ea89aa8b8f7d30d71f97a28b6cb9d
helscn <helscn@163.com>
2014-01-23 15:11:47 +0800

    1.分离代码与网页内容 2.制定Point对象和Points对象
    Signed-off-by: helscn <helscn@163.com>

---
+++ js/functions.js
@@ -0,0 +1,139 @@
+﻿function transArr(arr,spec,n,scale){
+	if(n===undefined){n=4;}
+	spec=spec || 0;
+	scale=scale || 1;
+	var newArr=new Array();
+	var temp=Math.pow(10,n);
+	for (var i=0;i<arr.length;i++){
+		newArr.push(Math.round((arr[i]-spec)*scale*temp)/temp);
+	}
+	return newArr;
+}
+
+//将数据保留至指定位小数
+function roundTo(num,n,scale){
+	if(n===undefined){n=4;}
+	scale=scale || 1;
+	num*=scale;
+	var temp=Math.pow(10,n);
+	return Math.round(num*temp)/temp;
+}
+
+//定义数据点对象
+/*
+	.isScale							计算偏差量时是否对标准坐标拉伸
+	.name								数据点的名字字符串
+	.specX								设计X坐标
+	.specY								设计Y坐标
+	.measX								量测X坐标
+	.measY								量测Y坐标
+	.scaleX								X轴的拉伸系数
+	.scaleY								Y轴的拉伸系数
+	.biasX(zoomFactor)					X轴方向上量测坐标与设计坐标的偏差量，ZoomFactor为偏差量缩放系数
+	.biasY(zoomFactor)					Y轴方向上量测坐标与设计坐标的偏差量，ZoomFactor为偏差量缩放系数
+	.moveMeasPos(shiftX,shiftY)			平移设计坐标的坐标原点
+	.moveMeasPos(shiftX,shiftY)			平移量测坐标的坐标原点
+*/
+function Point(isScale,name,specX,specY,measX,measY,scaleX,scaleY){
+	this.isScale=new Boolean(isScale);
+	this.name=new String(markName);
+	this.specX=new Number(specX);
+	this.specY=new Number(specY);
+	this.measX=new Number(measX);
+	this.measY=new Number(measY);
+	this.scaleX=new Number(scaleX || 1);
+	this.scaleY=new Number(scaleY || 1);
+	return this;
+}
+
+//返回X轴偏差量
+Point.prototype.biasX=function (zoomFactor){
+	zoomFactor = zoomFactor || 1;
+	return this.isScale?((this.measX-this.specX*this.scaleX)*zoomFactor):((this.measX-this.specX)*zoomFactor);
+}
+
+//返回Y轴偏差量
+Point.prototype.biasY=function(zoomFactor){
+	zoomFactor = zoomFactor || 1;
+	return this.isScale?((this.measY-this.specY*this.scaleY)*zoomFactor):((this.measY-this.specY)*zoomFactor);
+}
+
+//平移标准坐标的坐标原点
+Point.prototype.moveSpecPos=function (shiftX,shiftY){
+	this.specX=this.specX-shiftX;
+	this.specY=this.specY-shiftY;
+}
+
+//平移量测坐标的坐标原点
+Point.prototype.moveMeasPos=function (shiftX,shiftY){
+	this.measX=this.measY-shiftX;
+	this.measY=this.measY-shiftY;
+}
+
+//定义数据点组合对象
+/*
+	.points								保存Point对象的Array
+	.name								数据点组的名称字符串
+	.moveSpecPos(shiftX,shiftY)			平移标准坐标原点
+	.moveMeasPos(shiftX,shiftY)			平移量测坐标原点
+	.setIsScale(isScale)				将所有数据点统一设置是否缩放
+	.setScale(scaleX,scaleY)			将所有数据点统一设置拉伸系数
+	.splitBy(propertyName)				将数据按照属性名称进行分组,对应属性的值会被保存到数据点组的name属性
+*/
+function Points(name){
+	this.points=new Array();
+	this.name=new String(name);
+}
+
+//平移标准坐标的坐标原点
+Points.prototype.moveSpecPos=function (shiftX,shiftY){
+	for (var i=0;i<this.points.length;i++){
+		this.points[i].specX=this.points[i].specX-shiftX;
+		this.points[i].specY=this.points[i].specY-shiftY;
+	}
+}
+
+//平移标准坐标的坐标原点
+Points.prototype.moveMeasPos=function (shiftX,shiftY){
+	for (var i=0;i<this.points.length;i++){
+		this.points[i].measX=this.points[i].measX-shiftX;
+		this.points[i].measY=this.points[i].measY-shiftY;
+	}
+}
+
+//统一设置是否拉伸坐标
+Points.prototype.setIsScale=function (isScale){
+	for (var i=0;i<this.points.length;i++){
+		this.points[i].isScale=new Boolean(isScale);
+	}
+}
+
+//统一设置拉伸系数
+Points.prototype.setScale=function (scaleX,scaleY){
+	for (var i=0;i<this.points.length;i++){
+		if(scaleX!==undefined){
+			this.points[i].scaleX=new Number(scaleX);
+		}
+		if(scaleY!==undefined){
+			this.points[i].scaleY=new Number(scaleY);
+		}
+	}
+}
+
+//将数据按照属性名称进行分组,对应属性的值会被保存到数据点组的name属性
+Points.prototype.splitBy(propertyName){
+	var tempArr=new Array();
+	
+}
+
+
+//比较数组中是否已经含有目标值
+Array.prototype.include=function(t){
+	var i=0;
+	while (i<this.length){
+		if(this[i]===t){return true;}
+		i++;
+	}
+	return false;
+}
+
---
+++ js/main.js
@@ -0,0 +1,172 @@
+﻿//定义全局变量
+var myApp = {
+	dhxLayout : new Object,
+	dhxGrid : new Object,
+	dhxToolbar1 : new Object,
+	dhxToolbar2 : new Object,
+	statusBar : new Object,
+	chart : new Object
+};
+
+var userData = {
+	gridRecords : new Array(),	//保存表格记录对象的数组
+	posData : new Array(),		//计算旋转角度及涨缩时用到的坐标数据
+	rowCount : 24   			//Grid的默认行数
+};
+
+$(document).ready(function() {
+	myApp.dhxLayout = new dhtmlXLayoutObject(document.body, "2U");
+
+	myApp.dhxLayout.cells("a").setWidth(390);
+	myApp.dhxLayout.cells("a").setText("数据输入");
+	myApp.dhxToolbar1 = myApp.dhxLayout.cells("a").attachToolbar();
+	myApp.dhxToolbar1.setIconsPath("./lib/imgs/toolbar/");
+	myApp.dhxToolbar1.loadXML("./data/dhxtoolbar.xml?" + new Date().getTime());
+	myApp.dhxGrid = myApp.dhxLayout.cells("a").attachGrid();
+	myApp.dhxGrid.setImagePath("./lib/imgs/");
+	myApp.dhxGrid.setHeader("是否拉伸,分组名称,设计坐标,#cspan,量测坐标,#cspan");
+	myApp.dhxGrid.attachHeader(["#rspan","#rspan","X坐标", "Y坐标",  "X坐标", "Y坐标"]);
+	myApp.dhxGrid.setInitWidths("40,70,65,65,65,65");
+	myApp.dhxGrid.setColAlign("center,center,right,right,right,right");
+	myApp.dhxGrid.setColTypes("ch,edtxt,edtxt,edtxt,edtxt,edtxt");
+	myApp.dhxGrid.enableValidation(true,true);
+	myApp.dhxGrid.setColValidators([,"NotEmpty","ValidNumeric","ValidNumeric","ValidNumeric","ValidNumeric"]);
+	myApp.dhxGrid.init();
+	for (var i=0;i<userData.rowCount;i++){
+		myApp.dhxGrid.addRow(i, [1]);
+	}
+
+	myApp.dhxLayout.cells("b").setText("形变量示意图");
+	myApp.dhxToolbar2 = myApp.dhxLayout.cells("b").attachToolbar();
+	myApp.dhxToolbar2.setIconsPath("./lib/imgs/toolbar/");
+	myApp.dhxToolbar2.loadXML("./data/dhxtoolbar.xml?" + new Date().getTime());
+	myApp.dhxLayout.cells("b").attachObject("chart");
+	myApp.dhxGrid.attachEvent("onLiveValidationError",function(id){
+		myApp.statusBar.setText(id);;
+	});
+	//myApp.dhxLayout.setEffect('collapse', true);
+	//myApp.dhxLayout.setEffect('resize', true);
+	myApp.statusBar = myApp.dhxLayout.attachStatusBar();
+	myApp.statusBar.setText("Simple Status Bar");
+	
+
+	
+	Highcharts.setOptions({
+		lang:{
+			resetZoom:'恢复原始大小',
+			resetZoomTitle:'取消视图缩放以显示所有数据.'
+		}
+	});
+    myApp.chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'chart',
+            type: 'line',
+			zoomType: 'xy'
+        },
+		title: {
+			text: '',
+			style: {
+				fontSize:'11pt'
+			}
+		},
+        xAxis: {
+            title: {
+				text: 'X轴'
+			},
+			gridLineDashStyle:'dash',
+			gridLineWidth:1
+        },
+        yAxis: {
+            title: {
+                text: 'Y轴'
+            },
+			gridLineDashStyle:'dash',
+			gridLineWidth:1
+        },
+        tooltip: {
+            formatter: function() {
+                return 'X: '+this.x+' mil<br />Y: '+this.y+' mil<br />'+'D: '+Math.round(Math.sqrt(this.x*this.x+this.y*this.y)*100)/100+' mil';
+            }
+        },
+        plotOptions: {
+			series:{
+				lineWidth:1,
+				radius:1,
+				lineWidth:1,
+				pointStart:1
+            }
+        },
+        series: [/*{
+            name: 'X1',
+            marker: {
+                symbol: 'circle'
+            },
+            data: [[100,2],[-300,4],null,[500,600],[-270,-300]]
+        }, {
+            name: 'X2',
+            marker: {
+                symbol: 'circle'
+            },
+            data: []
+        },{
+            name: 'Y1',
+            marker: {
+                symbol: 'circle'
+            },
+            data: []
+        },{
+            name: 'Y2',
+            marker: {
+                symbol: 'circle'
+            },
+            data: []
+        }*/]
+    });
+	
+	myApp.dhxLayout.attachEvent("onResizeFinish",function(){
+		refreshChartSize();
+	});
+	
+/*
+	$("[title]").qtip({
+		style:{
+			name:'blue',
+			tip:true
+		},
+		position:{
+			target:'mouse',
+			adjust:{
+				mouse:true,
+				screen:true,
+				x:0,
+				y:-5
+			}
+		},
+		show:{
+			effect:{
+				type:'fade',
+				length:300
+			}
+		},
+		hide:{
+			effect:{
+				type:'fade',
+				length:300
+			}
+		}
+	});
+*/
+});
+
+function refreshChartSize(){
+	//if (myApp.dhxLayout.cells("b").getWidth()<305) {
+	//	myApp.chart.container.style.width="300px";
+	//}else{
+		myApp.chart.container.style.width=myApp.dhxLayout.cells("b").getWidth()-5;
+	//}
+	//if (myApp.dhxLayout.cells("b").getHeight()<330) {
+	//	myApp.chart.container.style.height="300px";
+	//}else{
+		myApp.chart.container.style.height=myApp.dhxLayout.cells("b").getHeight()-65;
+	//}
+}
